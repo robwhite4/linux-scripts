010fae28eab892abb8b7215b2e6681d66070fc94
The" Rob White IV <robwhite4@users.noreply.github.com>
2018-03-24 11:42:06 -0500

    Create yarn-install-json-beautifier.sh

---
+++ vagrant/centos7/dev/v1/yarn-install-json-beautifier.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# This script uses Yarn to install the JSON Beautifier CLI utility
+# > https://github.com/andlrc/json-beautifier
+
+echo ""
+echo "[Provision-Script] Installing json-beautifier (via Yarn)"
+echo ""
+
+yarn global add json-beautifier
